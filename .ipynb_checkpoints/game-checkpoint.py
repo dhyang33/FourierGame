83b79d6ebcea6a17af9c1bdccffb2006f2665dc8
Daniel Yang <dhyang@hmc.edu>
2020-11-27 01:34:36 -0800

    Add first minigame

--- .ipynb_checkpoints/game-checkpoint.py
+++ .ipynb_checkpoints/game-checkpoint.py
@@ -1,36 +1,14 @@
 import pygame
 import argparse
+from pitchGame import *
 parser = argparse.ArgumentParser()
-parser.add_argument('-d','--difficulty',dest = "difficulty",action="store", help='difficulty level')
+#parser.add_argument('-d','--difficulty',dest = "difficulty",action="store", help='difficulty level')
 
-def main():
-    pygame.init()
-
-    # Set up the drawing window
-    screen = pygame.display.set_mode([500, 500])
-
-    # Run until the user asks to quit
-    running = True
-    while running:
-
-        # Did the user click the window close button?
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        # Fill the background with white
-        screen.fill((255, 255, 255))
-
-        # Draw a solid blue circle in the center
-        pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
-        # Flip the display
-        pygame.display.flip()
-
-    # Done! Time to quit.
-    pygame.quit()
-    
+def main(difficulty):
+    freqs = [262]
+    game = PitchGame(difficulty)
+    game.run()
     
 if __name__ == "__main__":
     args = parser.parse_args()
-    main()
+    main(3)
---
+++ sprite.py
@@ -0,0 +1,44 @@
+import pygame
+class Sprite:
+    rect = None
+    image = None
+    imagePos = None
+    rectColor = None
+    origColor = None
+    text = None
+    textpos = None
+    timer = -1
+    count = 0
+    def __init__(self,rect=None, image=None, imagePos=None, rectColor=None, text = None, textPos = None, textColor = None):
+        self.rect = rect
+        self.image = image
+        self.imagePos = imagePos
+        self.rectColor = rectColor
+        self.text = text
+        self.textPos = textPos
+        self.textColor = textColor
+    
+    def timedColorChange(self,newColor,numFrames):
+        self.timer = numFrames
+        self.count = 0
+        self.origColor = self.rectColor
+        self.rectColor = newColor
+            
+    def drawSprite(self,screen):
+        if self.rect!=None:
+            pygame.draw.rect(screen, self.rectColor, self.rect)
+        if self.image != None:
+            screen.blit(self.image, self.imagePos)
+        if self.text != None:
+            myfont = pygame.font.SysFont('Arial', 50)
+            screen.blit(myfont.render(self.text, False, self.textColor), self.textPos)
+            
+    
+    def update(self):
+        if self.timer!=-1:
+            if self.count < self.timer:
+                self.count+=1
+            else:
+                self.rectColor = self.origColor
+                self.count = 0
+                self.timer = -1
